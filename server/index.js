cdc4d3f577063a4d86658d0122022a9cd5b72ca2
yashwanth17230 <yashwanthr110@gmail.com>
2024-06-04 19:04:45 +0530

    create a server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app=express();
+
+//const port = process.env.PORT || 5000;
+
+app.listen(3000, () =>{
+    console.log(`Server is running on port 3000`);
+    }
+    
+ );
