8dfb5b7343c51ef4b178bb4b8df1e0ac5a8d4399
yashwanth17230 <yashwanthr110@gmail.com>
2024-06-05 23:06:00 +0530

    create a middleware function to handle error

--- server/index.js
+++ server/index.js
@@ -21,3 +21,13 @@ app.use(express.json());
 
   app.use("/server/user",userRouter);
   app.use("/server/auth",authRouter);
+
+  app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message =err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success:false,
+      statusCode,
+      message,
+    });
+  });
