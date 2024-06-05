dc22fe7ed00dbd8e17941adbd5c556335d9c5033
yashwanth17230 <yashwanthr110@gmail.com>
2024-06-05 23:29:09 +0530

    create a sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,22 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function SignOut() {
+export default function SignUp() {
   return (
-    <div>
-      SIGN UP
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text 6xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4 '>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'/>
+        <input type='text' placeholder='email' className='border p-3 rounded-lg' id='email'/>
+        <input type='text' placeholder='password' className='border p-3 rounded-lg' id='password'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opactity-95 disabled:opacity-80'>sign up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>sign in</span>
+        </Link>
+      </div>
     </div>
+    
   )
 }
