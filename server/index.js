5a02f1b4b6e1839152beee0abe1cb859719267f6
yashwanth17230 <yashwanthr110@gmail.com>
2024-06-05 22:50:14 +0530

    create signup and api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -13,9 +14,10 @@ mongoose
     console.log(err);
   });
 const app =express();
+app.use(express.json());
   app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
 
   app.use("/server/user",userRouter);
-
+  app.use("/server/auth",authRouter);
